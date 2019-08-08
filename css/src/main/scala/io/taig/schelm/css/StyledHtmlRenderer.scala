c284c3154fd567c0aeb340b1b08dc546a633b664
Niklas Klein <mail@taig.io>
2019-08-08 20:59:55 +0200

    Remove unused Node param

--- css/src/main/scala/io/taig/schelm/css/StyledHtmlRenderer.scala
+++ css/src/main/scala/io/taig/schelm/css/StyledHtmlRenderer.scala
@@ -4,13 +4,13 @@ import cats._
 import cats.implicits._
 import io.taig.schelm._
 
-final class StyledHtmlRenderer[F[_]: Functor, Event](
-    renderer: Renderer[F, Html[Event], Reference[Event]]
-) extends Renderer[F, StyledHtml[Event], StyledReference[Event]] {
+final class StyledHtmlRenderer[F[_]: Functor, A](
+    renderer: Renderer[F, Html[A], Reference[A]]
+) extends Renderer[F, StyledHtml[A], StyledReference[A]] {
   override def render(
-      styled: StyledHtml[Event],
+      styled: StyledHtml[A],
       path: Path
-  ): F[StyledReference[Event]] =
+  ): F[StyledReference[A]] =
     renderer.render(toHtml(styled), path).map { reference =>
       val stylesheet = toStylesheet(styled)
       StyledReference(reference, stylesheet)
@@ -18,8 +18,8 @@ final class StyledHtmlRenderer[F[_]: Functor, Event](
 }
 
 object StyledHtmlRenderer {
-  def apply[F[_]: Monad, Event, Node](
-      renderer: Renderer[F, Html[Event], Reference[Event]]
-  ): Renderer[F, StyledHtml[Event], StyledReference[Event]] =
-    new StyledHtmlRenderer[F, Event](renderer)
+  def apply[F[_]: Monad, A](
+      renderer: Renderer[F, Html[A], Reference[A]]
+  ): Renderer[F, StyledHtml[A], StyledReference[A]] =
+    new StyledHtmlRenderer[F, A](renderer)
 }
